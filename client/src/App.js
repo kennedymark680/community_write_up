833fb3ae46f45556b3dd5fa22d32d6daa05bddb7
Mark Kennedy <kennedymark@hotmail.com>
2022-03-28 15:10:31 -0400

    Creating routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" />
+        <Route path="/List_in_Section" />
+        <Route path="/Details" />
+        <Route path="/Discrepancy" />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
