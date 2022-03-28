53bd45196f7ebd94eac43671f621386dc65525ff
Mark Kennedy <kennedymark@hotmail.com>
2022-03-28 15:06:11 -0400

    Starting over and installing the react app again.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return
-  ;<div className="App">
-    <Routes>
-      <Route path="/" element="" />
-      <Route path="/list_in_section" element="" />
-      <Route path="/discrepancy_form" element="" />
-      <Route path="/details" element="" />
-    </Routes>
-  </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
