49c7fc39a0804ced996d9711c052691f311a3571
Mark Kennedy <kennedymark@hotmail.com>
2022-03-28 16:18:09 -0400

    Passing form values down as props.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,54 @@
 import { Route, Routes } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Home from './pages/Home'
 import DiscrepancyForm from './pages/Discrepancy_Form'
 
 function App() {
+  const [formValue, setFormValue] = useState({
+    title: '',
+    date: '',
+    aircraft: '',
+    description: '',
+    open: '',
+    section: '',
+    image: ''
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    const newValue = (prevState) => {
+      return {
+        ...prevState,
+        [name]: value
+      }
+    }
+    setFormValue(newValue)
+  }
+
+  const { title, date, aircraft, description, open, section, image } = formValue
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/List_in_Section" />
         <Route path="/Details" />
-        <Route path="/Discrepancy" element={<DiscrepancyForm />} />
+        <Route
+          path="/Discrepancy"
+          element={
+            <DiscrepancyForm
+              title={title}
+              date={date}
+              aircraft={aircraft}
+              description={description}
+              open={open}
+              section={section}
+              image={image}
+              handleChange={handleChange}
+            />
+          }
+        />
       </Routes>
     </div>
   )
