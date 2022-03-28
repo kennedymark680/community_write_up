062fe14d901725699d412162df1677a1b5125bf2
Mark Kennedy <kennedymark@hotmail.com>
2022-03-28 14:04:53 -0400

    Created Routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return
+  ;<div className="App">
+    <Routes>
+      <Route path="/" element="" />
+      <Route path="/list_in_section" element="" />
+      <Route path="/discrepancy_form" element="" />
+      <Route path="/details" element="" />
+    </Routes>
+  </div>
 }
 
 export default App
