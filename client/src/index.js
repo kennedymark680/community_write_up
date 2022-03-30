09e9b14b5324fe4b8079f509c4384cd4cd428e7a
Mark Kennedy <kennedymark@hotmail.com>
2022-03-30 10:40:31 -0400

    Ipmorted font

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+// import WebFont from 'webfontloader';
+// import { useEffect } from 'react'
 
 ReactDOM.render(
   <BrowserRouter>
