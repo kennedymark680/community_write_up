f0369ea3726b1e4ce09858daeba9f5681c8ac665
Mark Kennedy <kennedymark@hotmail.com>
2022-03-30 11:33:59 -0400

    Animation on the home page.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,25 +3,44 @@ import Nav from '../components/Nav'
 
 const Home = (props) => {
   return (
-    <div>
+    <div className="home-page">
       <Nav />
-      <div className="container">
-        <h1 className="typed-out">Community Write-Up</h1>
-      </div>
+      <div className="home-body">
+        <div className="container">
+          <h1 className="typed-out">Community Write-Up</h1>
+        </div>
 
-      <img src="https://i.ibb.co/ry4zLrq/ec130.png" alt="ec130" border="0" />
-      <input
-        className="form"
-        type="text"
-        name="search"
-        placeholder="Search"
-        value={props.searchQuery}
-        onChange={props.handleSearchChange}
-      />
-      <button onClick={() => console.log('Searched')}>Search</button>
-      <Link to={`/list_in_section/${props.aircraftIds[0]}`}>
-        <button>EC130</button>
-      </Link>
+        <img
+          src="https://i.ibb.co/X8097DF/ec130.png"
+          alt="ec130"
+          border="0"
+          className="helicopter"
+        />
+        <img
+          src="https://i.ibb.co/mGCdjBc/shadow.png"
+          alt="shadow"
+          border="0"
+          className="shadow"
+        />
+        <div className="aircraft-buttons">
+          <Link to={`/list_in_section/${props.aircraftIds[0]}`}>
+            <button>EC130</button>
+          </Link>
+          <button>H300</button>
+          <button>BK117</button>
+        </div>
+      </div>
+      <div className="search-section">
+        <input
+          className="form"
+          type="text"
+          name="search"
+          placeholder="Search"
+          value={props.searchQuery}
+          onChange={props.handleSearchChange}
+        />
+        <button onClick={() => console.log('Searched')}>Search</button>
+      </div>
     </div>
   )
 }
