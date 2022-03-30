01615e619cbdfdbdbb292a5c6ddb9cf8d9ba615c
Mark Kennedy <kennedymark@hotmail.com>
2022-03-30 12:35:35 -0400

    Styleing delete button

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -24,9 +24,9 @@ const Card = (props) => {
           <h1>{props.title}</h1>
           <h3>{props.date}</h3>
           {/* <img src={props.image} alt='discrepancy'/> */}
-          <button onClick={() => props.deleteDiscrep(props.id)}>Delete</button>
-          <button onClick={() => props.updateDiscrep(props.id)}>{status}</button>
-          <button onClick={() => showDetails()}>Details</button>
+          <button className="card-Button" onClick={() => props.deleteDiscrep(props.id)}>Delete</button>
+          <button className="card-Button" onClick={() => props.updateDiscrep(props.id)}>{status}</button>
+          <button className="card-Button" onClick={() => showDetails()}>Details</button>
 
       
     </div>
